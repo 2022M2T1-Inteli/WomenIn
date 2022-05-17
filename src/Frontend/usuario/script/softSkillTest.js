da1e94f2e72da0df4924f5f01997b7de578af9df
Paulo Presa Evangelista <paulo.evangelista@sou.inteli.edu.br>
2022-05-16 23:56:15 -0300

    softskillstest

--- src/Frontend/usuario/script/softSkillTest.js
+++ src/Frontend/usuario/script/softSkillTest.js
@@ -1,7 +1,52 @@
-var slider = document.getElementById("OI");
-var output = document.getElementsByClassName("column");
-output.innerHTML = slider.value; // Display the default slider value
+const slidersArr = document.getElementsByClassName("form-range");
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;}
+document.querySelector("body").addEventListener("touchend", function (event) {
+  if (event.srcElement.nodeName == "INPUT") {
+    let currentSlider = event.srcElement.id;
+    value = document.getElementById(currentSlider).value;
+    console.log(currentSlider, value);
+    const [selectedH2] = $("#" + currentSlider).next();
+    switch (value) {
+      case "-2":
+        selectedH2.textContent = "Discordo";
+        break;
+      case "-1":
+        selectedH2.textContent = "Discordo parcialmente";
+        break;
+      case "0":
+        selectedH2.textContent = "neutro";
+        break;
+      case "1":
+        selectedH2.textContent = "Concordo parcialmente";
+        break;
+      case "2":
+        selectedH2.textContent = "Concordo";
+        break;
+    }
+  }
+});
+document.querySelector("body").addEventListener("mouseup", function (event) {
+  if (event.srcElement.nodeName == "INPUT") {
+    let currentSlider = event.srcElement.id;
+    value = document.getElementById(currentSlider).value;
+    console.log(currentSlider, value);
+    const [selectedH2] = $("#" + currentSlider).next();
+    switch (value) {
+      case "-2":
+        selectedH2.textContent = "Discordo";
+        break;
+      case "-1":
+        selectedH2.textContent = "Discordo parcialmente";
+        break;
+      case "0":
+        selectedH2.textContent = "neutro";
+        break;
+      case "1":
+        selectedH2.textContent = "Concordo parcialmente";
+        break;
+      case "2":
+        selectedH2.textContent = "Concordo";
+        break;
+    }
+  }
+});

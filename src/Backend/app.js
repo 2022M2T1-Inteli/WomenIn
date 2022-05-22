dca2a8b7857df38ebef3ca8b522dd9db1d45ac49
Paulo Presa Evangelista <paulo.evangelista@sou.inteli.edu.br>
2022-05-22 00:49:58 -0300

    ..

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -6,33 +6,45 @@ const app = express();
 const hostname = "127.0.0.1";
 const port = 3030;
 const sqlite3 = require("sqlite3");
+const { stringify } = require("querystring");
 const db = new sqlite3.Database("database.db");
 app.use(bp.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static("../Frontend/"));
 
-app.post("/signup", (req, res) => {
+app.post("/signup", (req, res1) => {
   const infos = req.body;
-  console.log(infos.name, infos.email, infos.password);
   db.run(
     `INSERT INTO users(name, email, password) VALUES('${infos.name}', '${infos.email}', '${infos.password}')`,
-    (response) => {
-      console.log(response);
-      if (response == null) {
-        res.sendFile(
+    (res2) => {
+      if (res2 == null) {
+        db.get(
+          `SELECT id FROM users WHERE email == '${infos.email}'`,
+          (err, res3) => {
+            if (err != null)
+              res1.send("ERRO! Tivemos um problema em pegar seu ID!");
+            else {
+              let currentId = res3["id"];
+              res1.cookie("name", currentId);
+              console.log(String(currentId));
+            }
+          }
+        );
+        res1.sendFile(
           path.resolve(
             __dirname + "/../Frontend/usuario/pages/dashboardUser.html"
           )
         );
-      } else if (response.code == "SQLITE_CONSTRAINT") res.send("ERRO!");
+      } else if (res2.code == "SQLITE_CONSTRAINT")
+        res1.send("ERRO! E-mail já utilizado ou banco de dados ocupado!");
     }
   );
 });
 
-// app.get("/", (req, res) => {
-//   console.log("Responding - /");
-//   res.sendFile(path.resolve(__dirname + "/../Frontend/index.html"));
-// });
+app.get("/dashboard", (req, res) => {
+  console.log("!!!!!!!!!!!!!");
+  res.send("ok!");
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);

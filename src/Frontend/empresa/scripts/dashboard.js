dcaa0a6cfd1bc205ed7f101c2994db7397ff78d3
Paulo Presa Evangelista <paulo.evangelista@sou.inteli.edu.br>
2022-06-20 15:01:53 -0300

    .

--- src/Frontend/empresa/scripts/dashboard.js
+++ src/Frontend/empresa/scripts/dashboard.js
@@ -20,28 +20,30 @@ const getInfo = async () => {
 };
 
 getInfo().then((res) => {
-  if (res.profileName) {
-    $("#infoEmpresa").append(
+  console.log(res)
+  if (res) {
+    $("#content").append(
       `
-      <div class="infos">
-        <div class="nome">
-            <h2>${res.profileName}</h2>
+      <div class="nameDescription">
+          <div class="nome">
+            <h1>${res.profileName}</h1>
+          </div>
+          <div class="bio">
+            <p>${res.bio}</p>
+          </div>
         </div>
-        <div class="location">
-             <h1>Sede da empresa: ${res.companyCity}, ${res.companyState}</h1>
-        </div>     
-        <div class="site">
-             <h1>Site: ${res.website}</h1>
+        <div class="sedeLink">
+          <div class="location">
+            <p><b>Sede da empresa:</b> ${res.companyCity}, ${res.companyState}</p>
+          </div>
+          <div class="site">
+            <p><b>Site:</b> <a href="${res.website}">${res.website}</a></p>
+          </div>
         </div>
-      </div>
-      <div class="bio">
-        <h2> ${res.bio} </h2>
-      </div>
-
       `
     );
   } else {
-    $("#infoEmpresa").append(
+    $("#content").append(
       `
             <h4 class="local">Você ainda não terminou seu perfil</h4>
             <a href="/editarEmpresa">
